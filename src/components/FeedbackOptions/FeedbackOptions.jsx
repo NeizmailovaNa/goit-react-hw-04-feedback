2214cf2a4716645067521a1f616e904c9d0d0480
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-03-06 10:52:35 +0200

    main code

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return Object.keys(options).map(el => {
+    return (
+      <button 
+        type="button"
+        onClick={() => {
+          onLeaveFeedback(el);
+        }}
+        key={el}
+      >
+        {el}
+      </button>
+    );
+  });
+};
+
+export default FeedbackOptions;
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
